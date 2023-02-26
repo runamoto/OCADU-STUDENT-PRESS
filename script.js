d723deabe3f0eb174f1e564432d30aae80496ae8
Omama Mahmood <98207789+runamoto@users.noreply.github.com>
2023-02-25 20:07:00 -0500

    this works

--- script.js
+++ script.js
@@ -1,75 +1,60 @@
-// this is where you initialise the wrapper, put in the slug here
-// what do i mean by slug: If for example you got to a channel and the link is:
-// https://www.are.na/aaryan-pashine/models-of-conversation
-// the slug is models-of-conversation
-// So go ahead and pass that to the class, you will have to add a / in the end cuz I coded it poorly
-let client = new Arena("ocadu-minful-campus/"); //<---
+let client = new Arena("ocadu-minful-campus/");
+const blocks = $(".panel");
 
-// Now to use it there are a bunch of things you can do with the wrapper
-// so if i say client.texts() it will basically get me all the blocks in the channel that are texts, similarly to get all the images i would say client.images() and so on
-client.texts().then((response) => {
-  console.log(response);
-});
-// if you go over to the console you can see the results, it will get the all the data for each block show you an array of it.
-// There is a then() function here which you might not be familiar with but basically when working with getting data from the web in javascripts we use something called promises, you can search up a coding train video to understand it better, but all you need to work with the wrapper is that:
-// whatever you put in your () in .then((res) => {
-//   is the data that you can reference and do stuff with, using javascript
-// })
-// So lets see how we can take all the text blocks it returns and add it to the webpage. Here I named the variable blocks, cuz we can name it anything right and it makes sense to call it blocks cuz we're getting multiple blcks. Yes I know I'm like really smart <3
+client.allContent().then((content) => setupContent(content));
 
-client.texts().then((blocks) => {
-  // we know that blocks is an array right, like you can see in console.log
-  // so we can run a for loop and in that each will be a single block
-  for (const block of blocks) {
-    // so im running a loop here where everytime it runs itll be a one of the blocks in the channel
-    // and then I can just sequentially add them to the webpage
+function setupContent(data) {
+  data.map((item) => {
+    var div = document.createElement("div");
+    // console.log(item);
 
-    // to do this using jquery all you have to so is reference the dom element using $("") and then use all the functions available to u to do whatever you wanna do. So in our case we will you append to add the p element with the content we get from are.na
-    $(".panel").append(block.content_html);
+    if (item.class == "Text") {
+      div.classList.add("text");
+      div.innerHTML = `
+                  ${item.content_html}
+              `;
+    } else if (item.class == "Link") {
+      div.classList.add("link");
+      div.innerHTML = `
+                  <a href="${item.source.url}">${item.source.url}</a>
+              `;
+    } else if (item.class == "Image") {
+      div.classList.add("image");
+      div.innerHTML = `
+                  <figure><img class="article-img" src="${item.image.original.url}"/></figure>
+                  <caption>${item.content}</caption>
+              `;
+    } else if (item.class == "Attachment") {
+      if (item.attachment.content_type == "video/mp4") {
+        div.classList.add("video");
+        div.innerHTML = `
+                      <video controls>
+                          <source src="${item.attachment.url}" type="video/mp4">
+                      </video>
+                  `;
+      }
+    }
 
-    // you can go in your console and see what all things you can use from the data you get from are.na
-  }
-});
+    if (item.title == "stylesheet") {
+      var css = item.content,
+        head = document.head || document.getElementsByTagName("head")[0],
+        style = document.createElement("style");
 
-client.texts().then((blocks) => {
-  for (const block of blocks) {
-    $(".panel").append(`
-        <div>
-          ${block.content}
-          <p style="
-          font-style: normal;
-              width: 200px">
-            Added by ${block.connected_by_username}</p>
-        </div>
-      `);
-  }
-});
+      head.appendChild(style);
 
-// Bonus
+      style.type = "text/css";
+      if (style.styleSheet) {
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+      } else {
+        style.appendChild(document.createTextNode(css));
+      }
+    } else {
+      blocks.append(div);
+    }
+  });
+}
+
 $(".panel").on("click", () => {
-  $(".panel").toggleClass("css-class-i-added-to-transform");
-});
-
-// another example comment it in to see what happens
-// client.texts().then((blocks) => {
-//   for (const block of blocks) {
-//     $(".panel").append(`
-//       <div>
-//         ${block.content}
-//         <p style="
-//             font-size: 8px;
-//             background-color: yellow;
-//             width: 100px">
-//           Added by ${block.connected_by_username}</p>
-//       </div>
-//     `);
-//   }
-// });
-
-client.images().then((blocks) => {
-  for (const block of blocks) {
-    $(".panel").append(
-      `<img class="article-img" src=${block.image.original.url}></img>`
-    );
-  }
+  $(".panel").toggleClass("out");
 });
